627bdfe5ff1970b5dc30a445e67633177fc04a3a
crosstec <markus.bopp@crosstec.org>
2021-08-07 13:17:19 +0200

    # fixed a display problem in details if the sale token is having 2 or less decimals

--- js/collectible-dapp.js
+++ js/collectible-dapp.js
@@ -130,11 +130,6 @@ function TncDapp() {
             let decimals = await tncLib.tokenDecimalsErc20(ask.tokenAddress);
             price = _this.formatNumberString(price, decimals);
 
-            if(price.length >= 10) {
-
-                price = price.substring(0, price.length - 10);
-            }
-
             rawPrice = price;
 
             if(decimals > 2) {
@@ -847,7 +842,7 @@ function TncDapp() {
                         }
                     );
                 }
-p
+                p
                 found = true;
                 break;
             }
@@ -1281,33 +1276,33 @@ p
     };
 
     this.cleanUpDecimals = function (price) {
-      price = _this.removingDecimals(price);
+        price = _this.removingDecimals(price);
 
-      let decimalPoints = 0;
-      if (price.includes(".")) {
-        decimalPoints = price.split(".")[1].length;
-      }
+        let decimalPoints = 0;
+        if (price.includes(".")) {
+            decimalPoints = price.split(".")[1].length;
+        }
 
-      //So that we always have at least 2 zeroes after decimal point
-      if (decimalPoints == 0){
-        price = price + "00"
-      }
-      else if(decimalPoints == 1){
-          price = price + "0";
-      }
-    
-      return price
+        //So that we always have at least 2 zeroes after decimal point
+        if (decimalPoints == 0){
+            price = price + "00"
+        }
+        else if(decimalPoints == 1){
+            price = price + "0";
+        }
+
+        return price
     };
 
     this.removingDecimals = function (string) {
         if(string == '0') return '0';
-      while (true) {
-        if (string.slice(-1) == 0) {
-          string = string.substring(0, string.length - 1);
-        } else {
-          return string;
+        while (true) {
+            if (string.slice(-1) == 0) {
+                string = string.substring(0, string.length - 1);
+            } else {
+                return string;
+            }
         }
-      }
     };
 
     this.getUrlParam = function(param_name) {
